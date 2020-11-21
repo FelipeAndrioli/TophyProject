5a89d9622bbe3238d1795195938e8ac90eed3270
Felipe Andrioli <felipeandrioli56@hotmail.com>
2020-11-21 16:23:38 -0300

    Added window opening and triangle to show in the window and test

--- tophy_engine/Tophy/src/main.cpp
+++ tophy_engine/Tophy/src/main.cpp
@@ -3,13 +3,23 @@
 #include <stdlib.h>
 #include "../include/glfw3.h"
 #include "../include/glfw3native.h"
+#include "./graphics/window.h"
 
 int main (int argc, char* argv[]) {
+    using namespace tophy;
+    using namespace graphics;
 
-    if(!glfwInit()) {
-        std::cout << "Error" << std::endl;
-    } else {
-        std::cout << "Success" << std::endl;
+    Window window("Tophy", 800, 600);
+    glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+
+    while (!window.closed()) {
+        window.clear();
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+        window.update();
     }
 
     return 0;
